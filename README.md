e039df2126a74b32501922503a9b02dea2ea5a9b
teppokoivula <teppo@piikkilanka.com>
2014-03-28 22:57:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# WARNING: EARLY PROOF OF CONCEPT RELEASE, NOT FOR PRODUCTION USE!
+
+THIS MODULE IS A WORK IN PROGRESS. DO NOT USE THIS MODULE FOR ANYTHING APART
+FROM TESTING PURPOSES. IT'S ALSO MORE THAN LIKELY THAT MANY DETAILS INCLUDING
+(BUT NOT LIMITED TO) DATABASE STRUCTURE USED MAY CHANGE BEFORE FINAL RELEASE,
+RESULTING IN LOSS OF DATA.
+
+FOR PRODUCTION READY VERSION CONTROL MODULE FOR PROCESSWIRE PLEASE CHECK OUT
+VERSION CONTROL FOR TEXT FIELDS, WHICH CAN BE FOUND FROM PROCESSWIRE MODULES
+DIRECTORY.
+
+Version Control For ProcessWire CMS/CMF
+=======================================
+
+Version Control Module For ProcessWire CMS/CMF.
+Copyright (c) 2013-2014 Teppo Koivula
+
+This module uses hooks provided by ProcessWire to catch page edits, finds out
+which fields were changed and attempts to store those values for later use.
+
+Note that at module settings you can define which specific fieldtypes and fields
+to track and for which templates tracking values should be enabled. Right out of
+the box tracking is disabled for all templates and fields.
+
+While editing a page with version control enabled, revision toggle (link which
+opens list of previous revisions) is shown for fields with earlier revisions
+available. From this list user can select a revision and rollback value of
+the field to that specific revision.
+
+## Supported fieldtypes and inputfields
+
+All native ProcessWire fieldtypes and inputfields, apart from those that either
+don't directly store values at all (repeaters fields, fieldsets) or only store
+hashed values (password), should be supported by the time of this writing:
+
+  * Email
+  * Datetime
+  * Text (regular and multi-language)
+  * Textarea (regular and multi-language)
+  * Page Title (regular and multi-language)
+  * Checkbox
+  * Integer
+  * Float
+  * URL
+  * Page
+  * Module
+  * File
+  * Image
+  
+These fieldtypes are confirmed to work properly with this module. If fieldtype
+isn't listed here, it doesn't necessarily mean that it's not supported - just
+that it hasn't been tested yet. If you know a fieldtype which works properly
+but isn't included here, please inform the author of this module via GitHub.
+
+Following inputfields are confirmed to be supported:
+
+  * TinyMCE
+  * CKEditor (regular and inline mode)
+  * Text (+ other inputfields using `<input>` HTML element, such as Email)
+  * Textarea (+ other inputfields using regular `<textarea>` HTML element)
+  * Select
+  * File
+  * Image
+
+## Getting started
+
+Copy (or clone with git) VersionControl folder to /site/modules/, go to Admin >
+Modules, hit "Check for new modules" and install Version Control. Supporting
+modules Process Version Control and Page Snapshot are installed automatically.
+
+After installing this module you need to configure it before anything really
+starts happening. Most configuration options (essentially templates and fields
+this module is switched on for) can be found from Admin > Modules > Version
+Control (module config.) Minor settings can be found from related Process
+modules config: Admin > Modules > Process Version Control.
+
+## Diff Match and Patch
+
+The Diff Match and Patch libraries offer robust algorithms to perform the
+operations required for synchronizing plain text. In the scope of current
+module, the JavaScript implementation of Diff Match and Patch is used to
+render diff between different revisions of a field value.
+
+Diff Match and Patch is copyright (c) 2006 Google Inc. and released under
+the Apache License, Version 2.0. For more information about this library,
+please visit http://code.google.com/p/google-diff-match-patch/.
+
+## License
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+(See included LICENSE file for full license text.)
---
+++ VersionControl.js
@@ -0,0 +1,252 @@
+$(function() {
+
+    // configuration: "run-time" settings are defined here, "constant" settings
+    // (translations, interface URL etc.) in VersionControlForTextFields.module
+    var settings = { empty: true, render: 'HTML' };
+    var moduleConfig = config.VersionControlForTextFields;
+
+    // field data is cached to reduce need for redundant AJAX requests
+    var cache = {};
+
+    // fetch revision data for this page as HTML markup
+    $.get(moduleConfig.processPage, { pages_id: moduleConfig.pageID, settings: settings }, function(data) {
+        
+        // prepend data (#text-field-history) to body
+        $('body').prepend(data);
+
+        // iterate through field specific revision containers and add their
+        // contents to that fields header (.ui-widget-header) only if they
+        // contain at least one revision other than what's currently used
+        $('#text-field-history > div').each(function() {
+            var $if = $('.Inputfield_'+$(this).data('field'));
+            $if.find('> label')
+                .addClass('with-history')
+                .before($(this)); 
+            cache[$(this).data('revision')] = $if.find('div.ui-widget-content, .InputfieldContent').clone(true, true);
+            $(this).find('a:first').addClass('ui-state-active');
+        });
+        
+        // iterate through history-enabled fields to add a revision toggle
+        $('.ui-widget-header.with-history, .InputfieldHeader.with-history').each(function() {
+            var toggle_class = "field-revisions-toggle";
+            var toggle_title = "";
+            if ($(this).siblings('.field-revisions').find('li').length < 1) {
+                toggle_class += " inactive";
+                toggle_title = " title='"+$(this).siblings('.field-revisions').text()+"'";
+            }
+            var revisions_toggle = '<a '+toggle_title+'class="'+toggle_class+'"><span class="ui-icon ui-icon-clock"></span></a>';
+            if ($(this).find('.ui-icon').length) {
+                $(this).find('.ui-icon').after(revisions_toggle);
+            } else {
+                $(this).prepend(revisions_toggle);
+            }
+        });
+        
+        // when a link in revision list is clicked, fetch data for appropriate
+        // revision from the interface (most of the code here is related to how
+        // things are presented, loading animation etc.)
+        $('.field-revisions a').bind('click', function() {
+            if ($(this).hasClass('ui-state-active')) return false;
+            var settings = {};
+            var $this = $(this);
+            var $if = $this.parents('.Inputfield:first');
+            var field = $this.parents('.field-revisions:first').data('field');
+            $if.find('.field-revisions .ui-state-active').removeClass('ui-state-active');
+            $this.addClass('ui-state-active');
+            var $content = $if.find('div.ui-widget-content, .InputfieldContent');
+            var $loading = $('<span class="field-revisions-loading"></span>').hide().css({
+                height: $content.innerHeight()+'px',
+                backgroundColor: $content.css('background-color')
+            });
+            if ($if.hasClass('InputfieldDatetime')) {
+                // datetime inputfield has <p> tag around it from which we must
+                // remove margin-top here to to avoid odd (Webkit) CSS quirk
+                // (ProcessWire commit 2298dc0035751ad940cac48fd2a1129585c9581f
+                // removes said tag, but older versions still need this fix)
+                $content.find('input:first').parent('p').css('margin-top', 0);
+            } else if ($if.hasClass('InputfieldPage') || $if.hasClass('InputfieldSelect') || $if.hasClass('InputfieldImage') || $if.hasClass('InputfieldFile')) {
+                // for some inputfield types we need to get pre-rendered markup
+                // (HTML) instead of raw data as JSON
+                settings = { render: 'Input' };
+            }
+            var revision = $(this).data('revision');
+            if (cache[revision]) {
+                if (revision == $this.parents('.field-revisions:first').data('revision')) {
+                    // current (latest) revision is the only one we've stored
+                    // inputfield content as a jQuery object in our cache
+                    $content.replaceWith(cache[revision].clone(true, true));
+                    if ($if.find('.InputfieldFileList').length) {
+                        // for file inputs we need to reload InputfieldFile.js
+                        // in order to reset the HTML5 AJAX file upload
+                        $.getScript(config.urls.modules+"Inputfield/InputfieldFile/InputfieldFile.js");
+                    }
+                    if ($if.hasClass('InputfieldImage')) {
+                        // for image inputs we need to reload InputfieldImage.js
+                        // in order to reset Magnific Popup
+                        $.getScript(config.urls.modules+"Inputfield/InputfieldImage/InputfieldImage.js");
+                    }
+                } else {
+                    update($if, $content, settings, cache[revision]);
+                }
+            } else {
+                $content.css('position', 'relative').prepend($loading.fadeIn(250));
+                $.get(moduleConfig.processPage+'get', { id: $this.data('revision'), settings: settings }, function(data) {
+                    cache[revision] = data;
+                    update($if, $content, settings, cache[revision]);
+                    $loading.fadeOut(350, function() {
+                        $(this).remove();
+                    });
+                });
+            }
+            return false;
+        });
+
+        // this function updates inputfield content based on inputfield and
+        // content objects, settings (render mode etc.) and data (HTML or JSON)
+        var update = function($if, $content, settings, data) {
+            if (settings.render == "Input") {
+                // format of returned data is HTML
+                var before = $content.children('p.description:first');
+                var after = $content.children('p.notes:first');
+                $content.html(data).prepend(before).append(after);
+                if ($if.hasClass('InputfieldImage') || $if.hasClass('InputfieldFile')) {
+                    // make sure that grid mode (for images) is not enabled
+                    $if.removeClass('InputfieldImageGrid');
+                    // image and file data isn't editable until it has been
+                    // restored; we're using an overlay to avoid confusion
+                    $content.prepend('<div class="version-control-overlay"></div>');
+                    $('.version-control-overlay')
+                        .attr('title', moduleConfig.i18n.editDisabled)
+                        .bind('click', function() {
+                            alert($(this).attr('title'));
+                        })
+                        .hover(
+                            function() { $(this).parent('.overlay-parent').addClass('hover') }, 
+                            function() { $(this).parent('.overlay-parent').removeClass('hover') }
+                        )
+                        .parent('.InputfieldContent')
+                        .addClass('overlay-parent');
+                }
+            } else {
+                // format of returned data is JSON
+                $.each(data, function(property, value) {
+                    var language = property.replace('data', '');
+                    if (language) language = "__"+language;
+                    if (typeof tinyMCE != "undefined" && tinyMCE.get('Inputfield_'+field+language)) {
+                        // TinyMCE inputfield
+                        tinyMCE.get('Inputfield_'+field+language).setContent(value);
+                    } else if ($if.find('.InputfieldCKEditorInline').length) {
+                        // CKeditor inputfield in inline mode
+                        $if.find('.InputfieldCKEditorInline').html(value);
+                    } else if (typeof CKEDITOR != "undefined" && CKEDITOR.instances['Inputfield_'+field+language]) {
+                        // CKEditor inputfield
+                        CKEDITOR.instances['Inputfield_'+field+language].setData(value);
+                    } else if ($if.find('textarea').length) {
+                        // Textarea inputfield (or any other inputfield using
+                        // <textarea> HTML element)
+                        $if.find('textarea[name='+field+language+']').html(value);
+                    } else {
+                        // Text inputfield (or any other inputfield using
+                        // <input> HTML element)
+                        var $input = $if.find('input[name='+field+language+']');
+                        if ($input.hasClass('hasDatepicker')) $input.datepicker("setDate", new Date(value));
+                        else if ($if.hasClass('InputfieldCheckbox')) $input.prop("checked", value == $input.val() ? true : false);
+                        else $input.val(value);
+                    }
+                });
+            }
+        }
+        
+        // when mouse cursor is moved on a revisions toggle (or it is clicked,
+        // to make it accessible for touch devices etc.) show (or hide if it 
+        // was already visible) revision list
+        $('.field-revisions-toggle').bind('click mouseenter', function() {
+            if ($(this).hasClass('inactive')) return false;
+            var $revisions = $(this).parent('label').siblings('.field-revisions');
+            var show = ($revisions.is(':visible')) ? false : true;
+            $('.field-revisions').slideUp();
+            if (show) $revisions.slideDown();
+            return false;
+        });
+
+        // hide revision list when user moves mouse cursor off it; timeout
+        // and sticky class are fixes to an issue where moving cursor over
+        // absolutely positioned .compare-revisions within parent element
+        // (.field-revisions) triggered mouseleave event of parent itself
+        var revision_timeout;
+        $('.field-revisions').hover(function() {
+        	if (revision_timeout) {
+        		clearTimeout(revision_timeout);
+        		revision_timeout = false;
+        	}
+        }, function() {
+        	var $this = $(this);
+        	revision_timeout = setTimeout(function() {
+        		if (!$this.hasClass('sticky')) {
+        			$('.compare-revisions').remove();
+        			$this.slideUp();
+        		}
+        	}, 100);
+        });
+
+        // if <ul> element containing revision history is long enough to get
+        // vertical scrollbar, add some extra padding to compensate for it
+        $('.field-revisions > ul').each(function() {
+            // fetch DOM object matching current jQuery object (jQuery object
+            // doesn't have clientHeight or scrollHeight which we need here)
+            var dom_ul = $(this)[0];
+            // to get correct heights parent element needs to be visible (this
+            // should happen so fast that user never notices anything strange)
+            $(this).parent().show();
+            if (dom_ul.clientHeight < dom_ul.scrollHeight) {
+                $(this).addClass('scroll');
+            }
+            $(this).parent().hide();
+        });
+
+        // when mouse cursor is moved on a revision link, show compare/diff
+        // link, which -- when clicked -- loads a text diff for displaying
+        // differences between selected revision and current revision.
+        $('.field-revisions.diff li > a').bind('hover', function() {
+            $('.compare-revisions').remove();
+            if (!$(this).hasClass('ui-state-active')) {
+                // in this case r1 refers to current revision, r2 to selected
+                // revision. diff is fetched as HTML from revision interface.
+                var r1 = $(this).parents('.field-revisions:first').find('.ui-state-active').data('revision');
+                var r2 = $(this).data('revision');
+                var href = moduleConfig.processPage+'diff/?revisions='+r1+':'+r2;
+                var label = moduleConfig.i18n.compareWithCurrent;
+                $(this).before('<div class="compare-revisions"><a class="diff-trigger" href="'+href+'">'+label+'</a></div>');
+                // note: following (and some other actions in this file) could
+                // be achieved more efficiently with .on(), but since that was
+                // introduced in jQuery 1.7 and ProcessWire 2.2 only had 1.6.2
+                // that's not really an option quite yet.
+                $('.compare-revisions').hover(function() {
+                	$(this).parents('.field-revisions:first').addClass('sticky');
+                }, function() {
+                	$(this).parents('.field-revisions:first').removeClass('sticky');
+                })
+                $('.compare-revisions > a').bind('click', function() {
+                    var $parent = $(this).parent();
+                    var $loading = $('<span class="field-revisions-loading"></span>').hide().css({
+                        height: $parent.innerHeight()+'px',
+                        backgroundColor: $parent.css('background-color')
+                    });
+                    $parent.prepend($loading.fadeIn(250)).load($(this).attr('href'), function() {
+                        $(this).find('a.diff-trigger').remove();
+                        $(this).animate({
+                            width: '400px',
+                            padding: '14px'
+                        });
+                        $loading.fadeOut(350, function() {
+                            $(this).remove();
+                        });
+                    });
+                    return false;
+                });
+            }
+        });
+
+    });
+    
+});
