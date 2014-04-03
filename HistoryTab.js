1cd16498eca7ca7f9df1293d365d6d6a24a4c077
teppokoivula <teppo@piikkilanka.com>
2014-04-03 23:53:36 +0300

    Support for history tab filters

---
+++ HistoryTab.js
@@ -0,0 +1,43 @@
+$(function() {
+
+    // translations etc. are defined in VersionControl.module
+    var moduleConfig = config.VersionControl;
+
+    // edit comment feature
+    $('.edit-comment').bind('click', function() {
+        var $editLink = $(this);
+        var revision = $(this).data('revision');
+        var comment = prompt(moduleConfig.i18n.commentPrompt, $(this).prev('span').text());
+        if (comment !== null) {
+            $.post(moduleConfig.processPage+'comment', { revision: revision, comment: comment }, function(data) {
+                $editLink
+                    .prev('span')
+                        .text(data)
+                        .effect("highlight", {}, 500);
+            });
+        }
+        return false;
+    });
+
+    // update GET params when history filters are toggled
+    $('#history_filters select').bind('change', function() {
+        var params = "";
+        $(this).parents('#history_filters:first').find('input, select').each(function() {
+            if (params) params += "&";
+            params += $(this).attr('name') + "=" + $(this).val();
+        });
+        window.location.search = params;
+    });
+
+    // activate history tab if history filters were toggled
+    var $tab = $('#VersionControlHistory');
+    if ($tab.length && $tab.children('ul:first').data('active')) {
+        $('a#_' + $tab.attr('id')).trigger('click');
+    }
+
+    // remove history filters from Page Edit form on submit
+    $('form#ProcessPageEdit').bind('submit', function() {
+        $(this).find('#history_filters').remove();
+    });
+
+});
